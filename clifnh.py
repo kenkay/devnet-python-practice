bdf8a98d4356cad5d9c3493d9f7b02a3f4deacec
Ubuntu <dadmn@ubuntu.zcuua0cefzuejiss5dgcy2qksh.bx.internal.cloudapp.net>
2022-01-13 09:29:16 +0000

    object inheritance

---
+++ clifnh.py
@@ -0,0 +1,21 @@
+class flow:
+    def __init__(self) -> None:
+        pass
+
+    def passDown():
+        unit = input('required input ')
+        # unit = 'x'
+        # out = f'your value is {unit}'
+        # out = unit
+        return unit
+        # print (out)
+
+# x = print(flow.passDown(''))
+x = flow.passDown()
+
+class downProc(flow):
+    def prPoc():
+        # self.proc = proc 
+        print ('this is the entered value', x)
+
+y = downProc.prPoc()
