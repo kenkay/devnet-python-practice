c8fe76ef1d448b3712774a57c65dd9016fdf93ba
kenkay <kaloohken@gmail.com>
2022-01-14 08:48:33 +0000

    clean up

--- clifnh2.py
+++ clifnh2.py
@@ -1,25 +1,12 @@
 class flow:
     def __init__(self) -> None:
         pass
-
     def passDown():
-        # unit = input('required input ')
         unit = 5
         return unit
-        # out = f'your value is {unit}'
-        # out = unit
-        # print (unit)
-        # print (type(unit))
 
-# x = print(flow.passDown())
 z = flow.passDown()
-# print (type(x))
-
 
 class downProc():
     def prPoc():
-        # self.proc = proc 
         print (z)
-
-# y = downProc.prPoc()
-# print (type(y))
